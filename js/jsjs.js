7c3eec0336b4be37ad12202aed31773be73c96a9
Ramon Velarde <rvelarde83@hotmail.com>
2022-03-28 00:09:11 -0700

    Adds JS code improvements that show all the info needed on screen, styling missing

---
+++ js/jsjs.js
@@ -0,0 +1,15 @@
+const biography= {
+    "full-name": "Bartholomew Allen II",
+    "alter-egos": "Flash IV, Impulse",
+    "aliases": [
+      "-"
+    ],
+    "place-of-birth": "-",
+    "first-appearance": "-",
+    "publisher": "Flash IV",
+    "alignment": "good",
+  };
+
+  for (key in biography){
+      console.log(key+ " : "+biography[key])
+  }
--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ console.log("js live Ramono-project")
 const token = "10166261980010720";
 let $div = $("div");
 const $ul = $(".search-results")
+const $ulInfo = $("#info")
 const sResults = []
 let updatedUserSearch = "";
 const $input = $("input")
@@ -13,6 +14,8 @@ let $span = $("span")
 
 // put a click event on the button
 $button.on("click", () => {
+    $("#image").empty();
+    $("#info").empty();
 
     // get the text the user types
     const searchTerm = $input.val()
@@ -44,6 +47,8 @@ $button.on("click", () => {
                 $ul.append($li)
 
                 $li.on("click", (event) => {
+                    $("#image").empty();
+                    $("#info").empty();
                     console.log(event.target)
                     const newSearchItem = $(event.target)
                     console.log(newSearchItem.text() + " primero console log")
@@ -51,32 +56,62 @@ $button.on("click", () => {
                     for (i in data.results) {
                         if (data.results[i].id === idChar) {
                             console.log(data.results[i].powerstats.durability) // to see if I'm getting the info from the correct character
+                            const $img = $("<img id= image>");
+                            $img.attr("src", data.results[i].image.url);
+                            console.log(data.results[i].image.url)
+                            $img.appendTo("#image")
+                            const $bio = $("#info-character")
+                            const $h2 = $("<h2>")
+                            $h2.text(`Info of ${data.results[i].name}: `)
+                            $h2.appendTo("#image")
+                            for (const key in data.results[i].biography) {
+
+                                const $li2 = $("<li>")
+                                $li2.text(`${key} : ${data.results[i].biography[key]}`)
+                                $ulInfo.append($li2)
+                                console.log($li2.text())
+                            }
+                            for (const key in data.results[i].appearance) {
+
+                                const $li3 = $("<li>")
+                                $li3.text(`${key} : ${data.results[i].appearance[key]}`)
+                                $ulInfo.append($li3)
+                                console.log($li3.text())
+                            }
+                            for (const key in data.results[i].work) {
+
+                                const $li4 = $("<li>")
+                                $li4.text(`${key} : ${data.results[i].work[key]}`)
+                                $ulInfo.append($li4)
+                            }
+                            for (const key in data.results[i].connections) {
+
+                                const $li5 = $("<li>")
+                                $li5.text(`${key} : ${data.results[i].connections[key]}`)
+                                $ulInfo.append($li5)
+                                console.log($li5.text())
+                            }
+
+
+
                         }
+
                     }
 
 
-                    // $.ajax({
-                    //     url: `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/${token}/${idChar}`,
-
-                    // }).then((data) => {
-                    //     console.log(data)
-
-                    //     const $img = $("<img id= image>");
-                    //     $img.attr("src", data.image.url);
-                    //     console.log(data.image.url)
-                    //     $img.appendTo("#image")
-                    // })
 
 
                 })
 
             }
+
         }
-    
+
     })
     $input.val("");
     $span.text("");
     $(".search-results").empty();
+    // $("#image").empty();
 })
 
 
